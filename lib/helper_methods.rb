db308b073d0c57ac2f3ab4986b98124556f483be
cy2003 <catyyee@hotmail.com>
2017-08-06 13:33:46 -0400

    Created new_event_saved method

--- lib/helper_methods.rb
+++ lib/helper_methods.rb
@@ -42,6 +42,11 @@ def create_calendar
   @cal_name = Calendar.new(calendar_name)
 end
 
+def create_event
+  create_event_name
+  enter_start_time
+end
+
 def create_event_name
   puts "Enter a name for your new event"
   ev_name = gets.strip
@@ -115,9 +120,10 @@ def is_location
   location_input = gets.strip
   while true
     if location_input == "yes"
-      enter_location
+      enter_location_name
       break
     elsif location_input == "no"
+      new_event_saved
       break
     else
       puts "Format is incorrect. Is there a location?
@@ -127,8 +133,72 @@ def is_location
   end
 end
 
-def enter_location
+def enter_location_name
   puts "Enter location name"
   location_name = gets.strip
   @event.location[:name] = location_name
+  is_address
+end
+
+def is_address
+  puts "Is there an address?
+  - Enter 'yes' or 'no'"
+  address_input = gets.strip
+  while true
+    if address_input == "yes"
+      enter_location_address
+      break
+    elsif address_input == "no"
+      new_event_saved
+      break
+    else
+      puts "Format is incorrect. Is there an address?
+      - Enter 'yes' or 'no'"
+      address_input = gets.strip
+    end
+  end
+end
+
+def enter_location_address
+  puts "Enter location street address"
+  location_address = gets.strip
+  @event.location[:address] = location_address
+  enter_location_city
+end
+
+def enter_location_city
+  puts "Enter location city"
+  location_city = gets.strip
+  @event.location[:city] = location_city
+  enter_location_state
+end
+
+def enter_location_state
+  puts "Enter location state"
+  location_state = gets.strip
+  @event.location[:state] = location_state
+  enter_location_zip
+end
+
+def enter_location_zip
+  puts "Enter location zipcode"
+  location_zip = gets.strip
+  @event.location[:zip] = location_zip
+  new_event_created
+end
+
+def new_event_saved
+  puts "New Event Saved
+  #{@event.name}
+  - start time: #{@event.start_time}"
+  if @event.end_time
+    puts "- end time: #{@event.end_time}"
+  else
+    puts "  - All Day Event"
+  end
+  if !@event.location.empty?
+    puts"  - location: #{@event.location[:name]}, #{@event.location[:address]}, #{@event.location[:city]}, #{@event.location[:state]}, #{@event.location[:zip]}"
+  else
+    nil
+  end
 end
